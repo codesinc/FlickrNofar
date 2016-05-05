a30e0e62f265c0eb5da025c244bf49a45059996b
Nofar N <nofar@lightricks.com>
2016-05-05 15:40:47 +0300

    FlickrPhotosVC: Initial commit.
    This commit adds the FlickrPhotosViewController view controller
    that responsible of representing Flickr top place first image.

---
+++ FlickrPhotos/FlickrPhotos/FlickrPhotosViewController.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2016 Lightricks. All rights reserved.
+// Created by codesinc.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/// View controller that updates its image view to be the first image of the top place in Flickr.
+@interface FlickrPhotosViewController : UIViewController
+@end
+
+NS_ASSUME_NONNULL_END
